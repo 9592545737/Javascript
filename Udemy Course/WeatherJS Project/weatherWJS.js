1925d9fa17a8804473078f0db52718d84fadc410
Gurminder <gurminder037@gmail.com>
2020-03-07 17:05:33 -0800

    Current Changes

---
+++ Udemy Course/WeatherJS Project/weatherWJS.js
@@ -0,0 +1,21 @@
+class Weather{
+  constructor(city, state) {
+    this.apiID = '85b23013';
+    this.apikey = '8bd25154ab252f12ef39fb1d442d92a6';
+    this.city = city;
+    this.state = state;        
+  }
+
+  async getWeather() {
+    const response = await fetch(`http://api.weatherunlocked.com/api/trigger/{Location}/{DataType} {Element} {TimePeriod} {Operator} {Value}?app_id={APP_ID}&app_key={APP_KEY}${apiID}&${apikey}.json`);
+
+    const responseData = await response.json();
+    return responseData;
+  }
+
+  // Change Location 
+  changeLocation(city, state) {
+    this.city = city;
+    this.state = state;
+  }
+}
